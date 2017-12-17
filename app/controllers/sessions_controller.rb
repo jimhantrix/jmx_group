2b21a63910696e6a82bae15fea742006ca30af8c
jimhantrix <jimtekanyo@gmail.com>
2017-12-16 23:22:39 -0800

    created session params and updated session helpers in application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+	def login(user)
+
+		session[:user_id] = user.id 
+		@current_user = user 
+
+	end 
+
+
+	def current-user 
+
+		@current_user || User.find(session[:user_id])
+
+	end 
+
+	def logout 
+
+		@current_user = session[:user_id] = nil 
+
+	end 
+
+	
 end
