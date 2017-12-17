09ebb2a00577097ee86b980ff9b24a4106e983a0
jimhantrix <jimtekanyo@gmail.com>
2017-12-16 23:04:03 -0800

    creating users and index view page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+	def index 
+
+		@Users = User.all
+
+	end 
+
+
+	# def new 
+
+	# 	render :new 
+
+	# end 
+
+
+
+	# def create 
+
+	# 	user = User.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password] )
+
+	# 	redirect_to users_path
+
+	# end 
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root "users#index"
+
+  get "'users" => "users#index", as: "users"
+
+  # get "/signup" => "users#new", as: "new_user"
+
+  # post "/users" => "users#create"
+
+  # get "/users/:id" => "users#show", as "user"
+  
+
 end
